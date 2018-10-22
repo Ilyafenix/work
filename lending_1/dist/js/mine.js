f8255028612c9f896f0c90b23c3b73d4d4eae24b
ilya fenix <ilya@ilifix.ru>
2018-10-22 21:27:13 +0300

    сделал якорные ссылки и анимацию

--- lending_1/dist/js/mine.js
+++ lending_1/dist/js/mine.js
@@ -1,5 +1,18 @@
+// анимация меню
+
 $('.menu-btn').on('click', function(e) {
     e.preventDefault;
     $(this).toggleClass('menu-btn_active');
     $('.menu-nav__link').toggleClass('menu-nav__link_active');
+});
+
+// скрипт плавного перехода по ссылкам
+
+ $(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
 });
