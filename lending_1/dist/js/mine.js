b5b93e09be2525a4589e30b7585471c7c619fd8b
ilya fenix <ilya@ilifix.ru>
2018-10-27 11:18:30 +0300

    начал работу над слайдером
    составил набросок слайдера

--- lending_1/dist/js/mine.js
+++ lending_1/dist/js/mine.js
@@ -16,3 +16,25 @@ $('.menu-btn').on('click', function(e) {
         $('body,html').animate({scrollTop: top}, 1000);
     });
 });
+
+// получаем переменные для слайдера
+let sliderImg = document.getElementById("slider-img"),
+    dotsWrap = document.getElementsByClassName("slider-dots")[0],
+    dots = document.getElementsByClassName("dot"),
+    left = -400,
+    imgWidth = -200;
+// стартовая позиция для слайдера
+sliderImg.style.left = left + 'px';
+
+// функция перелистывания
+slideTo = (direction) => {
+    if (direction == "left") {
+        left = left - imgWidth;
+    }
+    if (direction == "right") {
+        left = left + imgWidth;
+    }
+    if (left < -800) left = 0;
+    if (left > 0) left = -800;
+    sliderImg.style.left = left + 'px';
+}
